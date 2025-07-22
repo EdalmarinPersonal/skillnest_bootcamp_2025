45b0b9b23da1f55f060b62b326149823ed6425f8
Edgar Marin <edalmarin@gmail.com>
2025-07-21 21:47:36 -0400

    js

--- 01prebootcamp/Edgar_Marin_JSPrediccion.js
+++ 01prebootcamp/Edgar_Marin_JSPrediccion.js
@@ -3,3 +3,31 @@ function miEdad() {
     console.log("Tengo: " + 25 + " años");
 
 }
+//Predicción 1. imprime en la consola: Tengo 25 años
+
+function miEdad(edad) {
+
+    console.log("Tengo: " + edad + " años");
+
+}
+//Predicción 2. imprime en la consola: Tengo 25 años
+
+function restar(primerNumero, segundoNumero) {
+
+    console.log("¡Restemos los números!");
+
+    console.log("primerNumero es:" + primerNumero);
+
+    console.log("segundoNumero es:" + segundoNumero);
+
+    var resultado = primerNumero - segundoNumero;
+
+    console.log(resultado);
+
+}
+
+//Prediccion 3:
+//¡Restemos los números!
+//primerNumero es:50
+//segundoNumero es:27
+//23
