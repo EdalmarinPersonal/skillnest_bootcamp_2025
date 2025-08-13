805e039e696178e5b3d9267ca7d7ca81b11e5a65
Edgar Marin <edalmarin@gmail.com>
2025-08-12 22:23:44 -0400

    bs

---
+++ bootstrap/script.js
@@ -0,0 +1,11 @@
+function cambiaTexto(element,texto){
+    element.innerText = texto;
+}
+
+function desapareceBtn(element){
+    element.remove();
+}
+
+function msajeSesion(){
+    alert("Bienvenido a la tienda de Flores")
+}
